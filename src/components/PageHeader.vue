b428ece9d04f2e22b19272e4e421b9780bd06ef3
Andrea Fermo <andreafermo.sd@gmail.com>
2023-04-11 13:04:09 +0200

    logo e navbar

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,6 +1,19 @@
 <template>
     <div class="container">
-        <nav></nav>
+        <div class="logo-wrapper">LOGO</div>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Me</a></li>
+                <li><a href="#">Testimonials</a></li>
+                <li><a href="#">My Blog</a></li>
+                <li><a href="#">Meetups</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">Contact Me</a></li>
+                <li><a href="#">C</a></li>
+
+            </ul>
+        </nav>
         <div class="jumbotron"></div>
         <div class="jumbotron-right-section"></div>
     </div>
@@ -12,13 +25,49 @@ export default {
 }
 </script>
 <style scoped lang="scss">
+@use '../styles/partials/variables';
+
 .container {
-    height: 800px;
-    width: 100%;
+    height: 450px;
+    width: 1100px;
     margin: auto;
     background-image: url(../assets/images/hero-07-2x.jpg);
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
+    position: relative;
+
+    .logo-wrapper {
+        height: 80px;
+        width: 80px;
+        background-color: pink;
+        position: absolute;
+        left: 100px;
+        top: 20px;
+
+    }
+
+    nav {
+        margin-right: 70px;
+
+        ul {
+            list-style: none;
+            display: flex;
+            justify-content: flex-end;
+
+            li {
+                padding: 0px 10px;
+
+                a {
+                    text-decoration: none;
+                    color: variables.$color-white;
+                    font-size: 12px;
+                    padding-top: 20px;
+                    display: block;
+                    border-top: 3px solid variables.$color-saffron;
+                }
+            }
+        }
+    }
 }
 </style>
