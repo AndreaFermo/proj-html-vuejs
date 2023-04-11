1a859be13fe10ec042a57917232f6bb3deda780f
Andrea Fermo <andreafermo.sd@gmail.com>
2023-04-11 11:52:47 +0200

    suddivisione aree progetto

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,49 @@
-<script setup>
+<template>
+  <header>
+    <nav></nav>
+    <div class="jumbotron"></div>
+    <div class="jumbotron-right-section"></div>
+  </header>
+  <main>
+    <section id="meet-author">
+
+    </section>
+
+    <section id="platforms">
+
+    </section>
+
+    <section id="audiobook">
+
+    </section>
+
+    <section id="critics">
+
+    </section>
+
+    <section id="events">
+
+    </section>
+
+    <section id="newsletter">
+
+    </section>
+
+    <section id="author-info"> <!-- simile alla section events-->
+
+    </section>
+
+    <section id="recent-articles"> <!-- simile alla section critics-->
+
+    </section>
+  </main>
+  <footer>
+    <!-- simile al footer di dc-comics-->
+  </footer>
+</template>
+<script >
 
 </script>
-
-<template></template>
-
 <style lang="scss">
 @use './styles/general.scss' as *;
 @use './styles/partials/variables';
